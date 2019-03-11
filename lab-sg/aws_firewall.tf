95439f2b92ded4c86172210d0319848f7b109a93
chau.bao.long <chau.bao.long@framgia.com>
2019-03-11 11:17:15 +0700

    done security groups lab.

---
+++ lab-sg/aws_firewall.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "sg_web" {
+  name = "ec2-sg"
+  description = "sg for ec2"
+  vpc_id = "${aws_vpc.vpc_main.id}"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "lab-architect-sg-web"
+  }
+}
